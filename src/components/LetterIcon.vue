2f58b53e3e069ba5997f0b99439ffa8665c2ce7c
zlayine <zlayine@enjin.io>
2024-04-13 13:38:42 +0000

    dark_mode

---
+++ src/components/LetterIcon.vue
@@ -0,0 +1,17 @@
+<template>
+    <div
+        class="letter__icon"
+        :style="{
+            backgroundColor: '#7373bf',
+            color: 'white',
+        }"
+    >
+        <span>{{ letter }}</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    letter: string;
+}>();
+</script>
